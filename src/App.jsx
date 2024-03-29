ab919a2db8818b22580618e903c2c41fdcb995e4
345257 <sbrrllss500@gmail.com>
2024-03-29 15:16:40 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,66 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import PokeCard from './components/PokeCard'
+import { useDebounce } from './hooks/useDebounce'
 
 function App() {
   const [pokemons, setPokemons] = useState([])
-
-  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0'
+  const [offset, setOffset] = useState(0)
+  const [limit, setLimit] = useState(20)
+  const [searchTerm, setSearchTerm] = useState("")
+  const debouncedSearchTerm = useDebounce(searchTerm, 500)
 
   useEffect(() => {
-    fetchPokeData()
+    fetchPokeData(true)
   }, [])
 
-  const fetchPokeData = async () => {
+  useEffect(() => {
+    handleSearchInput(debouncedSearchTerm)
+  }, [debouncedSearchTerm])
+
+  const fetchPokeData = async (isFirstFetch) => {
     try {
+      const offsetValue = isFirstFetch ? 0 : offset + limit
+      const url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offsetValue}`
       const response = await axios.get(url)
-      setPokemons(response.data.results)
+      setPokemons([...pokemons, ...response.data.results])
+      setOffset(offsetValue)
     }catch(error) {
       console.error(error)
+    }
+  }
+
+  const handleSearchInput = async (searchTerm) => {
+    if(searchTerm.length > 0) {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
+        const pokemonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`, 
+          name: searchTerm
+        }
+        setPokemons([pokemonData])
+      }catch(error) {
+        setPokemons([])
+        console.error(error)
+      }
+    }
+    else {
+      fetchPokeData(true)
     }
   }
 
   return (
     <article className='pt-6'>
       <header className='flex flex-col gap-2 w-full px-4 z-50'>
-        input form
+        <div className='relative z-50'>
+          <form className='relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto'>
+            <input type='text' value={searchTerm} className='text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center' onChange={(e)=>setSearchTerm(e.target.value)}>
+            </input>
+            <button type='submit' className='text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700'>
+              검색
+            </button>
+          </form>
+        </div>
       </header>
       <section className='pt-6 flex flex-col justify-center items-center overflow-auto'>
         <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
@@ -41,6 +78,11 @@ function App() {
             )}
         </div>
       </section>
+      <div className='text-center'>
+        <button className='bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white' onClick={() => {fetchPokeData(false)}}>
+          더 보기
+        </button>
+      </div>
     </article>
   )
 }
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import LazyImage from "./LazyImage"
 
 const PokeCard = ({url, name}) => {
   
@@ -29,22 +30,23 @@ const PokeCard = ({url, name}) => {
     return PokeData
   }
 
-  const bg = `bg-${pokemon?.type}`
-  const border = `bg-${pokemon?.type}`
-  const text = `bg-${pokemon?.type}`
+  const bg = `bg-${pokemon?.types}`
+  // const border = `border-${pokemon?.types}`
+  const text = `text-${pokemon?.types}`
   const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`
 
   return (
     <>
       {
         pokemon && 
-        <a href={`/pokemon/${name}`} className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}>
+        // <a href={`/pokemon/${name}`} className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}>
+        <a href={`/pokemon/${name}`} className={`box-border rounded-lg w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}>
           <div className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}>
             #{pokemon.id.toString().padStart(3, '00')}
           </div>
           <div className={`w-full f-6 flex items-center justify-center`}>
             <div className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}>
-              <img src={img} alt={name} width="100%" className={`object-contain h-full`}></img>
+              <LazyImage url={img} alt={name}/>
             </div>
           </div>
           <div className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1`}>
